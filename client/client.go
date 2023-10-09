821fccb051e3d3de1f4fdab7cbd3de97e4db19c5
koenno <8572607+koenno@users.noreply.github.com>
2023-10-09 20:19:56 +0200

    move package client; parse integers column

--- client/client.go
+++ client/client.go
@@ -1,10 +1,14 @@
 package client
 
 import (
+	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -43,4 +47,39 @@ func (c Client) Send(req *http.Request) ([]byte, string, error) {
 	}
 
 	return payloadBytes, resp.Header.Get("content-type"), nil
+}
+
+func ParseIntegers(bb []byte, contentType string) ([]int, error) {
+	if !validContentType(contentType) {
+		return nil, fmt.Errorf("unsupported content type: %s", contentType)
+	}
+
+	var integers []int
+	bytesReader := bytes.NewReader(bb)
+	bufReader := bufio.NewScanner(bytesReader)
+	for bufReader.Scan() {
+		if err := bufReader.Err(); err != nil {
+			return nil, fmt.Errorf("failed to parse integers: %v", err)
+		}
+		line := bufReader.Text()
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		integer, err := strconv.Atoi(line)
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert line to int: %s: %v", line, err)
+		}
+		integers = append(integers, integer)
+	}
+
+	return integers, nil
+}
+
+func validContentType(contentType string) bool {
+	if contentType == "" {
+		return false
+	}
+	elems := strings.Split(contentType, ";")
+	return elems[0] == "text/plain"
 }
